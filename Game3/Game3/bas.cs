007cc7b07754f63529c5eb46c177a6e803321b35
Emma carlsson <emca97001@utb.vaxjo.se>
2018-09-14 13:57:12 +0200

    saker hände

---
+++ Game3/Game3/bas.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game3
+{
+    class bas
+    {
+        protected Vector2 pos;
+        protected Texture2D tex;
+
+        public void update() { }
+        public virtual void Draw (SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(tex, pos, Color.AliceBlue);
+        }
+        public Texture2D Tex
+        {
+            get { return tex; }
+            set { tex = value; }
+        }
+        public Vector2 Pos
+        {
+            get { return pos; }
+            set { pos = value; }
+
+
+        }
+}
